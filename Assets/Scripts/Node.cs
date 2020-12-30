0d1ba2aec60504dadc3ecb43978e1ae547899bc4
xFluke <mikoman1124@gmail.com>
2020-12-30 04:18:26 -0500

    Updated Node.cs to work with the Heap class

--- Assets/Scripts/Node.cs
+++ Assets/Scripts/Node.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Node
+public class Node : IHeapItem<Node>
 {
     public Vector3 worldPosition;
     public bool walkable;
@@ -12,6 +12,7 @@ public class Node
     public int gCost;
     public int hCost;
     public Node parent;
+    int heapIndex;
 
     public Node(Vector3 _worldPosition, bool _walkable, int _gridX, int _gridY) {
         worldPosition = _worldPosition;
@@ -26,4 +27,21 @@ public class Node
         }
     }
 
+    public int HeapIndex {
+        get {
+            return heapIndex;
+        }
+        set {
+            heapIndex = value;
+        }
+    }
+
+    public int CompareTo(Node other) {
+        int compare = fCost.CompareTo(other.fCost);
+
+        if (compare == 0) {
+            compare = hCost.CompareTo(other.hCost);
+        }
+        return -compare;
+    }
 }
